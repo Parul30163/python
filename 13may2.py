bd758583f1704f0bbddd0aca83d0e98644016ff6
Parul30163 <113704573+Parul30163@users.noreply.github.com>
2024-05-14 21:25:40 +0530

    Create 13may2.py

---
+++ 13may2.py
@@ -0,0 +1,52 @@
+#assignment day2
+'''def generate_n_chr():
+    for i in range(x):
+        print(y,end="")
+
+x=int(input("enter no."))
+y=str(input("enter alph.."))
+generate_n_chr()#output= ppppp
+
+#max no from list
+def max_in_list(x):
+    max_value= x[0]
+    
+    for i in x[1:]:
+        if (i> max_value):
+            max_value=i
+    return max_value
+
+x=[10,20,85,62]
+print(max_in_list(x))#output=> 85
+
+
+#length of string
+def word_len(lst):
+    length=[]
+    for i in lst:
+        length.append(len(i))
+    return length
+
+word=["manan","chinku","parul"]
+length= word_len(word)
+print(length)#output=> [5,6,5]
+
+#longest word length
+def lword_length(lst):
+    max_length=0
+    for word in lst:
+        if  len(word)> max_length:-
+            max_length=len(word)
+    return max_length    
+word=["manan","chinku","parul"]
+print(lword_length(word))#output=> 6 
+
+
+#LAMBDA FUNCTION
+ls = lambda word_list: max(len(word) for word in word_list)
+words = ["parul","manan","chinku"]
+print( ls(words))'''
+
+
+
+        
