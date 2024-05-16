ddd8ff67998a238b672ca9a02f7600e922873d4a
Parul30163 <113704573+Parul30163@users.noreply.github.com>
2024-05-16 11:50:56 +0530

    Create whatapp.py

---
+++ whatapp.py
@@ -0,0 +1,6 @@
+   
+import pywhatkit as kit
+
+receiver = "+919214420565"  # Receiver's phone number (including country code)
+message = "helloo byeee good nightt papa"
+kit.sendwhatmsg(receiver, message, 22, 42)  # Send the message at 9:00 AM
